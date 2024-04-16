bfc478aa5e36d9b24c4c05a6dd9cc24c2c78cb2b
JuanOcaranza <68519407+JuanOcaranza@users.noreply.github.com>
2024-04-15 21:20:54 -0300

    Polar added

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from datetime import datetime
+import numpy as np
 
 class Data:
     def __init__(self, df, objects):
@@ -7,6 +8,8 @@ class Data:
         self.objects = objects
         self._add_velocity()
         self._add_acceleration()
+        self.add_polar_wrist()
+        self.add_angular_velocity()
 
     def _add_velocity(self):
         for object in self.objects:
@@ -17,6 +20,21 @@ class Data:
         for object in self.objects:
             self.df[f"ax_{object}"] = self.df[f"vx_{object}"].diff()
             self.df[f"ay_{object}"] = self.df[f"vy_{object}"].diff()
+    
+    def add_polar_wrist(self):
+        x_vector_elbow_to_wrist = self.df['rx_wrist'] - self.df['rx_elbow']
+        y_vector_elbow_to_wrist = self.df['ry_wrist'] - self.df['ry_elbow']
+        magnitude_vector_elbow_to_wrist = np.sqrt(x_vector_elbow_to_wrist ** 2 + y_vector_elbow_to_wrist ** 2)
+        x_vector_elbow_to_shoulder = self.df['rx_shoulder'] - self.df['rx_elbow']
+        y_vector_elbow_to_shoulder = self.df['ry_shoulder'] - self.df['ry_elbow']
+        magnitude_vector_elbow_to_shoulder = np.sqrt(x_vector_elbow_to_shoulder ** 2 + y_vector_elbow_to_shoulder ** 2)
+        elbow_to_wrist_scalar_product_elbow_to_shoulder = x_vector_elbow_to_wrist * x_vector_elbow_to_shoulder + y_vector_elbow_to_wrist * y_vector_elbow_to_shoulder
+        cosine_angle = elbow_to_wrist_scalar_product_elbow_to_shoulder / (magnitude_vector_elbow_to_wrist * magnitude_vector_elbow_to_shoulder)
+        self.df['r_wrist'] = magnitude_vector_elbow_to_wrist
+        self.df['theta_wrist'] = np.arccos(cosine_angle)
+    
+    def add_angular_velocity(self):
+        self.df['angular_velocity'] = self.df['theta_wrist'].diff()
 
     def get_data(self, save = False):
         if save:
