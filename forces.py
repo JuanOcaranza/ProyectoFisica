e91ea883c681fce8580854b0b0bde6a27b3561ba
JuanOcaranza <68519407+JuanOcaranza@users.noreply.github.com>
2024-05-03 21:54:35 -0300

    Trying to add cosine to moment calculation

--- forces.py
+++ forces.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Forces:
     def __init__(self, df, mass_forearm, mass_weight, radius_bicep):
         self.df = df
@@ -13,10 +15,12 @@ class Forces:
         inertia_forearm = mass_forearm * radius_forearm ** 2
 
         sum_moment = (inertia_weight + inertia_forearm) * self.df['angular_acceleration']
-        moment_weight = radius_weight * mass_weight * g
-        moment_forearm = radius_forearm * mass_forearm * g
 
-        self.df['force_bicep'] = (sum_moment - moment_weight - moment_forearm) / radius_bicep
+        angle_forearm_g = np.pi - self.df['theta_wrist']
+        moment_weight = radius_weight * mass_weight * g * np.cos(angle_forearm_g)
+        moment_forearm = radius_forearm * mass_forearm * g * np.cos(angle_forearm_g)
+
+        self.df['force_bicep'] = (sum_moment - moment_weight - moment_forearm) / (radius_bicep * np.cos(self.df['theta_wrist']))
         
         self.df['sum_moment'] = sum_moment
         self.df['moment_weight'] = moment_weight
