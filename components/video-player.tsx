16c1eb1b4e766364611f92597340c005235d763d
JuanOcaranza <68519407+JuanOcaranza@users.noreply.github.com>
2024-06-04 07:42:57 -0300

    Revert "basic video player"
    This reverts commit e615be7a6c41e4022e6f4ef2f3022ba8667628a4.

--- components/video-player.tsx
+++
@@ -1,9 +0,0 @@
-export async function VideoPlayer({ path }: { path: string }) {
-
-    return (
-        <video controls aria-label="Video player">
-            <source src={"/videos/" + path + ".mp4"} type="video/mp4" />
-            Your browser does not support the video tag.
-        </video>
-    )
-}
